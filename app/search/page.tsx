34407d867607eed4445bf688e633f163de7d29b8
Erik-cmp <erikalvarezt08@gmail.com>
2023-10-16 16:07:19 +0700

    Readded Missing Files in Search Folder

---
+++ app/search/page.tsx
@@ -0,0 +1,46 @@
+import getSongsByTitle from "@/actions/getSongsByTitleAndAuthor";
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
+import getSongsByTitleAndAuthor from "@/actions/getSongsByTitleAndAuthor";
+
+interface SearchProps {
+  searchParams: {
+    title: string;
+    author: string;
+  }
+};
+
+export const revalidate = 0;
+
+const Search = async ({ searchParams }: SearchProps) => {
+  const songs = await getSongsByTitleAndAuthor(searchParams.title || searchParams.author);
+
+  return(
+    <div className="
+      bg-neutral-900
+      rounded-lg
+      h-[calc(100%-80px)]
+      w-full
+      overflow-hidden
+      overflow-y-auto
+    ">
+      <Header className="from-bg-neutral-900">
+        <div className="
+          mb-2
+          flex
+          flex-col
+          gap-y-6
+        ">
+          <h1 className="text-white text-3xl font-semibold">
+            Search
+          </h1>
+          <SearchInput />
+        </div>
+      </Header>
+      <SearchContent songs={songs}/>
+    </div>
+  )
+}
+
+export default Search;
