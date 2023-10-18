044a07de791242e470d2f22c20207962b96e51dc
Erik-cmp <erikalvarezt08@gmail.com>
2023-10-18 10:37:35 +0700

    Sync

--- app/(site)/error.tsx
+++ app/(site)/error.tsx
@@ -6,10 +6,10 @@ const Error = () => {
   return (
     <Box className="h-full flex items-center justify-center">
       <div className="text-neutral-400">
-        Whoops! Something went wrong :(
+        Whoops! Something went wrong &nbsp;:&#40;
       </div>
     </Box>
-  )
+  );
 };
 
 export default Error;
