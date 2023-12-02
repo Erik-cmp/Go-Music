f561067d077c8904a44e48f745cfde4953bd636d
Erik-cmp <erikalvarezt08@gmail.com>
2023-12-02 09:12:31 +0700

    Minor Footer Changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (  
     <div className="w-full md:pt-6 pt-4 md:px-6 px-4">
-      <div className="w-full bg-neutral-700 h-[1px] rounded-full"></div>
+      <div className="w-full bg-neutral-800 h-[1.25px] rounded-full"></div>
       <div className="w-full flex justify-end py-4 md:text-sm text-xs text-neutral-400">
         &copy; 2023, Go-Tunes. All Rights Reserved
       </div>
