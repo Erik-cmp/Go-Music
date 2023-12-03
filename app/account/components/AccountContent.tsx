08865249490b08389121ed735be9a8b3e0fc2034
Erik-cmp <erikalvarezt08@gmail.com>
2023-12-03 15:59:09 +0700

    More minor changes to Account Page

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -34,7 +34,7 @@ const AccountContent = () => {
   };
   
   return (  
-    <div className="mb-7 p-6 min-h-[55vh]">
+    <div className="mb-7 p-6 min-h-[60vh]">
       {!subscription && (
         <div className="flex flex-col gap-y-4">
           <p className="flex flex-col gap-y-4 text-2xl font-semibold">User Information:</p>
