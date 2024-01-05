e6ccd95b8fa953d9378bdbd305ccfec6d5528a20
Erik-cmp <erikalvarezt08@gmail.com>
2024-01-05 18:42:22 +0700

    Changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="w-full md:pt-6 pt-4 md:px-6 px-4">
       <div className="w-full bg-neutral-800 h-[1.25px] rounded-full"></div>
       <div className="w-full flex justify-end py-4 md:text-sm text-xs text-neutral-400">
-        &copy; 2023, Go-Tunes. All Rights Reserved
+        &copy; 2024, Go-Tunes. All Rights Reserved
       </div>
     </div>
   );
